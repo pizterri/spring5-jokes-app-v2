5d0887414571ea484c13448911b4b5d629b8f4bb
pizterri <sandro.alig@gmail.com>
2022-01-01 20:42:16 +0100

    Create Joke controller

---
+++ src/main/java/com/pizterri/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.pizterri.spring5jokesappv2.controllers;
+
+import com.pizterri.spring5jokesappv2.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "index";
+    }
+}
